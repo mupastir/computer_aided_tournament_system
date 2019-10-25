985697dd0bbde7158e49f01d2c0fb40f7cae3d0d
oleg <mupastir@gmail.com>
2019-10-25 15:56:06 +0300

    move uuid field to user's model

--- aided_tournament_system/user_auth/models.py
+++ aided_tournament_system/user_auth/models.py
@@ -1,5 +1,12 @@
+import uuid as uuid_lib
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    uuid = models.UUIDField(
+        db_index=True,
+        default=uuid_lib.uuid4,
+        editable=False
+    )
