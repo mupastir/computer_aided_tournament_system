0d8c3f72d5482b9adc3c173a7d6b16108f05d322
oleg <mupastir@gmail.com>
2019-10-28 09:05:48 +0200

    modified relationships in player and team refactored referee

--- aided_tournament_system/participant/models.py
+++ aided_tournament_system/participant/models.py
@@ -4,6 +4,7 @@ from enum import Enum
 from django.db import models
 from django.urls import reverse
 from django_countries.fields import CountryField
+from game.models import Game
 from user_auth.models import User
 
 
@@ -12,54 +13,50 @@ class Player(User):
         woman = ('w', 'Woman')
         man = ('m', 'Man')
 
-    slug = models.SlugField(unique=True, verbose_name='slug')
-    sex = models.CharField(
-        max_length=1,
-        choices=[x.value for x in SEXES],
-        verbose_name='sex'
-    )
-
-    country = CountryField(
-        blank_label='(select country)',
-        verbose_name='sex'
-    )
-    rating = models.IntegerField(
-        null=True,
-        verbose_name='rating'
-    )
+    slug = models.SlugField(unique=True,
+                            verbose_name='slug')
+    sex = models.CharField(max_length=1,
+                           choices=[x.value for x in SEXES],
+                           verbose_name='sex')
+    country = CountryField(blank_label='(select country)',
+                           verbose_name='country')
+    rating = models.IntegerField(null=True,
+                                 verbose_name='rating')
+    team = models.ManyToManyField('Team',
+                                  on_delete=models.SET_NULL,
+                                  verbose_name='team')
 
     def get_absolute_url(self):
-        return reverse('flavors:detail', kwargs={'slug': self.slug})
+        return reverse('flavors:detail',
+                       kwargs={'slug': self.slug})
 
 
 class Team(models.Model):
-    uuid = models.UUIDField(
-        db_index=True,
-        default=uuid_lib.uuid4,
-        editable=False,
-        primary_key=True
-    )
-    slug = models.SlugField(unique=True, verbose_name='slug')
-    title = models.CharField(
-        max_length=100
-    )
-    player = models.ForeignKey(
-        Player,
-        on_delete=models.CASCADE,
-        verbose_name='players'
-    )
-    rating = models.IntegerField(
-        null=True,
-        verbose_name='total team rating'
-    )
+    uuid = models.UUIDField(db_index=True,
+                            default=uuid_lib.uuid4,
+                            editable=False,
+                            primary_key=True)
+    slug = models.SlugField(unique=True,
+                            verbose_name='slug')
+    title = models.CharField(max_length=100)
+
+    rating = models.IntegerField(null=True,
+                                 verbose_name='total team rating')
+    active = models.BooleanField(verbose_name='is team playing')
 
     def get_absolute_url(self):
         return reverse('flavors:detail', kwargs={'slug': self.slug})
 
 
 class Referee(User):
-    pass
+    class Roles(Enum):
+        first = ('1st', 'First')
+        second = ('2nd', 'Second')
+        scorer = ('scr', 'Scorer')
+        line_judge = ('lnj', 'Line judge')
 
-
-class ScoreMarker(User):
-    pass
+    game = models.ManyToManyField(Game,
+                                  on_delete=models.SET_NULL)
+    role = models.CharField(max_length=3,
+                            choices=[x.value for x in Roles],
+                            verbose_name='role')
