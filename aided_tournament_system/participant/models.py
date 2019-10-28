f5c6641541a6f8db68400f137a972797a0061da3
oleg <mupastir@gmail.com>
2019-10-28 16:49:30 +0200

    edit serializer modified views refactored model with UUIDTimeStampModel

--- aided_tournament_system/participant/models.py
+++ aided_tournament_system/participant/models.py
@@ -1,18 +1,18 @@
-import uuid as uuid_lib
 from enum import Enum
 
 from competition.models import Application
 from django.db import models
 from django_countries.fields import CountryField
+from django_utils.models import UUIDTimeStampModel
 from game.models import Game
-from user_auth.models import User
 
 
-class Player(User):
+class Player(UUIDTimeStampModel):
     class GENDER(Enum):
         woman = ('w', 'Woman')
         man = ('m', 'Man')
 
+    user = models.UUIDField(verbose_name='users uuid', unique=True)
     gender = models.CharField(max_length=1,
                               choices=[x.value for x in GENDER],
                               verbose_name='sex')
@@ -21,15 +21,12 @@ class Player(User):
     rating = models.IntegerField(null=True,
                                  verbose_name='rating')
     team = models.ManyToManyField('Team',
+                                  blank=True,
+                                  null=True,
                                   verbose_name='team')
 
 
-class Team(models.Model):
-    uuid = models.UUIDField(db_index=True,
-                            default=uuid_lib.uuid4,
-                            editable=False,
-                            primary_key=True,
-                            verbose_name='uuid')
+class Team(UUIDTimeStampModel):
     title = models.CharField(max_length=100, verbose_name='title')
     rating = models.IntegerField(null=True,
                                  verbose_name='total team rating')
@@ -37,13 +34,14 @@ class Team(models.Model):
                                          verbose_name='competition')
 
 
-class Referee(User):
+class Referee(UUIDTimeStampModel):
     class Roles(Enum):
         first = ('1st', 'First')
         second = ('2nd', 'Second')
         scorer = ('scr', 'Scorer')
         line_judge = ('lnj', 'Line judge')
 
+    user = models.UUIDField(verbose_name='users uuid', unique=True)
     game = models.ManyToManyField(Game)
     role = models.CharField(max_length=3,
                             choices=[x.value for x in Roles],
