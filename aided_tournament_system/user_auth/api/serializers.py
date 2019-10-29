4ae14eb480cc114e2d362c92383dec86a680b705
oleg <mupastir@gmail.com>
2019-10-29 17:22:43 +0200

    refactored serializers

--- aided_tournament_system/user_auth/api/serializers.py
+++ aided_tournament_system/user_auth/api/serializers.py
@@ -1,3 +1,4 @@
+from rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
 
 from ..models import User
@@ -6,10 +7,31 @@ from ..models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['first_name',
+                  'last_name',
+                  'email']
 
 
-class UserCreateSerializer(serializers.ModelSerializer):
+class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email', 'birthdate']
+        fields = ['birthdate',
+                  'email',
+                  'first_name',
+                  'last_name']
+
+
+class UserRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+
+    def get_cleaned_data(self):
+        super().get_cleaned_data()
+        return {
+            'username': self.validated_data.get('username', ''),
+            'password1': self.validated_data.get('password1', ''),
+            'password2': self.validated_data.get('password2', ''),
+            'email': self.validated_data.get('email', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', '')
+        }
