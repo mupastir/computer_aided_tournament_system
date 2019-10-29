04091737aa6bbefea050e058a9975abcbbd741a6
oleg <mupastir@gmail.com>
2019-10-29 17:23:49 +0200

    add connection to services views add registration, login, logout views

--- aided_tournament_system/user_auth/api/views.py
+++ aided_tournament_system/user_auth/api/views.py
@@ -1,8 +1,16 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.permissions import AllowAny
+from aided_tournament_system.user_auth.models import User
+from allauth.socialaccount.providers.facebook.views import \
+    FacebookOAuth2Adapter
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.instagram.views import \
+    InstagramOAuth2Adapter
+from rest_auth.registration.views import RegisterView, SocialConnectView
+from rest_auth.views import LoginView, LogoutView
+from rest_framework.generics import ListAPIView, UpdateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from ..models import User
-from .serializers import UserCreateSerializer, UserSerializer
+from .serializers import (UserRegisterSerializer, UserSerializer,
+                          UserUpdateSerializer)
 
 
 class UserListAPIView(ListAPIView):
@@ -11,7 +19,30 @@ class UserListAPIView(ListAPIView):
     serializer_class = UserSerializer
 
 
-class UserCreateAPIView(CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = UserCreateSerializer
+class UserUpdateAPIView(UpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class UserLoginAPIView(LoginView):
+    pass
+
+
+class UserLogoutAPIView(LogoutView):
+    pass
+
+
+class UserRegisterAPIView(RegisterView):
+    serializer_class = UserRegisterSerializer
+
+
+class FacebookConnect(SocialConnectView):
+    adapter_class = FacebookOAuth2Adapter
+
+
+class GoogleConnect(SocialConnectView):
+    adapter_class = GoogleOAuth2Adapter
+
+
+class InstagramConnect(SocialConnectView):
+    adapter_class = InstagramOAuth2Adapter
