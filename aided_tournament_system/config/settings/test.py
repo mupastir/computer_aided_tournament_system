6eb20e82b5ace553743a09870811290657f0fadc
oleg <mupastir@gmail.com>
2019-10-30 16:59:45 +0200

    updated test setting

--- aided_tournament_system/config/settings/test.py
+++ aided_tournament_system/config/settings/test.py
@@ -1,14 +1,3 @@
-from .base import *
-
-DATABASES.update({
-    'test': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'USER': 'oleg',
-        'NAME': 'tournament_system_test',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-})
+from .local import *
 
 INSTALLED_APPS += ('behave_django',)
