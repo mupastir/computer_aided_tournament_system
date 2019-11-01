81fe458a1690d36f3559bc76a85c86f9145dd3bd
oleg <mupastir@gmail.com>
2019-11-01 17:50:16 +0200

    update import steps

--- aided_tournament_system/features/steps/app_steps.py
+++ aided_tournament_system/features/steps/app_steps.py
@@ -1,2 +1,2 @@
-from features.competition.steps import competition_list
-from features.user_auth.steps import auth, login, logout
+from features.competition.steps import (competition_list, competition_create)
+from features.user_auth.steps import (auth, login, logout)
