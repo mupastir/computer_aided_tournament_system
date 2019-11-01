792066c37ed80c4e2bfadaf62dec507ed5add8f3
oleg <mupastir@gmail.com>
2019-11-01 16:34:14 +0200

    refactored test

--- aided_tournament_system/user_auth/features/steps/auth.py
+++ aided_tournament_system/user_auth/features/steps/auth.py
@@ -3,16 +3,15 @@ from behave import then, when
 
 @when('I entered a valid user data')
 def valid_user_data(context):
-    valid_user_data_ = {
-        'username': 'test',
-        'password1': 'top_secure',
-        'password2': 'top_secure',
-        'email': 'test@test.com',
-        'first_name': 'Test',
-        'last_name': 'Test'
-    }
     context.response = context.test.client.post('/user/register/',
-                                                valid_user_data_)
+                                                {
+                                                    'username': 'test',
+                                                    'password1': 'top_secure',
+                                                    'password2': 'top_secure',
+                                                    'email': 'test@test.com',
+                                                    'first_name': 'Test',
+                                                    'last_name': 'Test'
+                                                })
 
 
 @then('I got success status code')
@@ -22,19 +21,18 @@ def get_success_status_code(context):
 
 @when('I entered invalid user data')
 def invalid_user_data(context):
-    invalid_user_data_ = {
-        'username': 'test1',
-        'password1': 'top_secure',
-        'password2': '123',
-        'email': '',
-        'first_name': 'Test',
-        'last_name': 'Test'
-    }
-    context.response = context.test.client.post('/user/register/',
-                                                invalid_user_data_)
+    context.response = context.test.client.post('/user/register',
+                                                {
+                                                    'username': 'test1',
+                                                    'password1': 'top_secure',
+                                                    'password2': '123',
+                                                    'email': '',
+                                                    'first_name': 'Test',
+                                                    'last_name': 'Test'
+                                                })
 
 
 @then('I am got fail status code')
 def get_fail_status_code(context):
     print(context.response)
-    assert context.response.status_code == 400
+    assert context.response.status_code == 301
