dec636310d541bb34333c4219857d4a13aafbed7
oleg <mupastir@gmail.com>
2019-11-04 16:19:10 +0200

    refactored with unique names urls

--- aided_tournament_system/competition/urls.py
+++ aided_tournament_system/competition/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(
         regex=r'^create/$',
         view=views.CompetitionCreateAPIView.as_view(),
-        name='competition_rest_api'
+        name='competition_create_rest_api'
     )
 ]
