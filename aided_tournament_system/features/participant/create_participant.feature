a809211a87344705080e430932a2ce19cef5d37e
oleg <mupastir@gmail.com>
2019-11-05 16:31:29 +0200

    init tests for participant creation views

---
+++ aided_tournament_system/features/participant/create_participant.feature
@@ -0,0 +1,30 @@
+# Created by oleg at 05.11.19
+Feature: Create participants
+  Check if teams, players and referee are created
+
+  Scenario: Create player
+    Given User with email: test@test.com and password: extra_strong, superuser: True, staff: True.
+    When User add player role
+    Then User got success status code
+    And User become a player
+
+  Scenario: Create team
+    Given User with email: test@test.com and password: extra_strong, superuser: True, staff: True.
+    When User create BestTeam
+    Then User got success status code
+
+  Scenario: Create referee
+    Given User with email: test@test.com and password: extra_strong, superuser: True, staff: True.
+    When User add referee role
+    Then User got success status code
+    And User become a referee
+
+  Scenario: Add players to team
+    Given User with email: test@test.com and password: extra_strong, superuser: True, staff: True.
+    And Player
+    |first_name|last_name|username      |email        |password   |
+    |Masha     |Test     |test1         |test@test.com|top_secure1|
+    And TEAM
+    When User add TEST_PLAYER to TEAM
+    Then User got success status code
+    And TEAM has TEST_PLAYER
---
+++ aided_tournament_system/features/participant/steps/create_participants.py
@@ -0,0 +1,73 @@
+from behave import given, then, when
+
+from features.utils import api_post
+from participant.models import Player, Referee, Team
+from user_auth.models import User
+
+
+@when('User add player role')
+def add_player(context):
+    context.response = api_post('/participant/player/create',
+                                context.user)
+
+
+@when('User add referee role')
+def add_referee(context):
+    context.response = api_post('/participant/referee/create',
+                                context.user)
+
+
+@when('User create {team_title}')
+def add_team(context, team_title):
+    context.response = api_post('/participant/team/create',
+                                context.user,
+                                {
+                                    'title': team_title
+                                })
+
+
+@given('Players')
+def create_players(context):
+    context.players = []
+    for row in context.table:
+        user = User.objects.create_user(username=row['username'],
+                                        email=row['email'],
+                                        password=row['password'],
+                                        first_name=row['first_name'],
+                                        last_name=row['last_name'])
+        context.players.append(Player.objects.create(user=user.id))
+
+
+@when('User add {player_username} to {team_title}')
+def add_players_to_team(context, player_username, team_title):
+    context.response = api_post(f'/participant/{team_title}/add_player',
+                                context.user,
+                                {
+                                    'player_username': player_username
+                                })
+
+
+@given('{team_title}')
+def create_team(context, team_title):
+    Team.objects.create(title=team_title)
+
+
+@then('{team_title} has {player_username}')
+def check_team_has_player(context, team_title, player_username):
+    player = Player.objects.all()
+    team = Team.objects.get(title=team_title)
+    assert True is False
+
+
+@then('User become a player')
+def check_player_exist(context):
+    user = context.user
+    players = Player.objects.all()
+    assert True is False
+
+
+@then('User become a referee')
+def check_referee_exist(context):
+    user = context.user
+    referees = Referee.objects.all()
+    assert True is False
