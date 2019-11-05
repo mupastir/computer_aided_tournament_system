65f4ae8a8cf462a56d0ccb1f1feae78519389764
oleg <mupastir@gmail.com>
2019-11-05 19:29:18 +0200

    refactored view creation games schedule

--- aided_tournament_system/competition/api/views.py
+++ aided_tournament_system/competition/api/views.py
@@ -1,10 +1,8 @@
 from competition.models import Competition
 from game.services.schedule_creation_services.schedule_creator import \
     ScheduleCreator
-from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import AllowAny, IsAdminUser
-from rest_framework.response import Response
 
 from .serializers import CompetitionCreateSerializer, CompetitionListSerializer
 
@@ -19,19 +17,9 @@ class CompetitionCreateAPIView(CreateAPIView):
     serializer_class = CompetitionCreateSerializer
     permission_classes = (IsAdminUser,)
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        title = request.data.get('title')
-        query_id = Competition\
-            .objects\
-            .filter(title=title)\
-            .values('id', 'schedule_system')
-        competition_id = query_id[0].get('id')
-        schedule_system = query_id[0].get('schedule_system')
-        tournament_games = ScheduleCreator(schedule_system, competition_id)
+    def perform_create(self, serializer):
+        super().perform_create(serializer)
+        competition = serializer.instance
+        tournament_games = ScheduleCreator(competition.schedule_system,
+                                           competition.id)
         tournament_games.create_schedule()
-        return Response(serializer.data, status=status.HTTP_201_CREATED,
-                        headers=headers)
