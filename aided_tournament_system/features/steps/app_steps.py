7ae3226de40405ccce8c61217dbbccb4df222517
oleg <mupastir@gmail.com>
2019-11-05 12:57:54 +0200

    add test for lists participants

--- aided_tournament_system/features/steps/app_steps.py
+++ aided_tournament_system/features/steps/app_steps.py
@@ -1,2 +1,3 @@
 from features.competition.steps import competition_create, competition_list
+from features.participant.steps import list_participants
 from features.user_auth.steps import auth, login, logout
