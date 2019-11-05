bb2902d36af62fc1e71e399ec79aed592f5d3d18
oleg <mupastir@gmail.com>
2019-11-05 16:34:28 +0200

    move environment file to features level refactored auth to force client

--- aided_tournament_system/features/user_auth/steps/auth.py
+++ aided_tournament_system/features/user_auth/steps/auth.py
@@ -1,35 +1,38 @@
 from behave import then, when
+from features.utils import api_post
 
 
 @when('I entered a valid user data')
 def valid_user_data(context):
-    context.response = context.test.client.post('/user/register/',
-                                                {
-                                                    'username': 'test',
-                                                    'password1': 'top_secure',
-                                                    'password2': 'top_secure',
-                                                    'email': 'test@test.com',
-                                                    'first_name': 'Test',
-                                                    'last_name': 'Test'
-                                                })
+    context.response = api_post('/user/register/',
+                                context.user,
+                                {
+                                    'username': 'test',
+                                    'password1': 'top_secure',
+                                    'password2': 'top_secure',
+                                    'email': 'test@test.com',
+                                    'first_name': 'Test',
+                                    'last_name': 'Test'
+                                })
 
 
-@then('I got success status code')
+@then('User got success status code')
 def get_success_status_code(context):
     assert context.response.status_code == 201
 
 
 @when('I entered invalid user data')
 def invalid_user_data(context):
-    context.response = context.test.client.post('/user/register',
-                                                {
-                                                    'username': 'test1',
-                                                    'password1': 'top_secure',
-                                                    'password2': '123',
-                                                    'email': '',
-                                                    'first_name': 'Test',
-                                                    'last_name': 'Test'
-                                                })
+    context.response = api_post('/user/register',
+                                context.user,
+                                {
+                                    'username': 'test1',
+                                    'password1': 'top_secure',
+                                    'password2': '123',
+                                    'email': '',
+                                    'first_name': 'Test',
+                                    'last_name': 'Test'
+                                })
 
 
 @then('I am got fail status code')
