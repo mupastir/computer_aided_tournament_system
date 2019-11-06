fab9a8b03996d5dca2d6c8ab83abd0b65a3bbee7
oleg <mupastir@gmail.com>
2019-11-06 17:10:02 +0200

    add game steps

--- aided_tournament_system/features/steps/app_steps.py
+++ aided_tournament_system/features/steps/app_steps.py
@@ -1,3 +1,4 @@
 from features.competition.steps import competition_create, competition_list
 from features.participant.steps import create_participants, list_participants
 from features.user_auth.steps import auth, login, logout
+from features.game.steps import protocolize_game
