fd00f82328b60ee6a0279324db4f96450b78ffd2
mupastir <mupastir@gmail.com>
2019-11-06 21:40:05 +0200

    add ids read only

--- aided_tournament_system/participant/api/serializers.py
+++ aided_tournament_system/participant/api/serializers.py
@@ -5,19 +5,22 @@ from rest_framework import serializers
 class PlayerListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = ['rating']
+        fields = ['id', 'rating']
+        read_only_fields = ['id']
 
 
 class RefereeListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Referee
-        fields = []
+        fields = ['id']
+        read_only_fields = ['id']
 
 
 class TeamListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['title', 'rating']
+        fields = ['id', 'title', 'rating']
+        read_only_fields = ['id']
 
 
 class PlayerInTeamListSerializer(serializers.ModelSerializer):
@@ -29,16 +32,19 @@ class PlayerInTeamListSerializer(serializers.ModelSerializer):
 class PlayerCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
-        fields = []
+        fields = ['id']
+        read_only_fields = ['id']
 
 
 class RefereeCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Referee
-        fields = []
+        fields = ['id']
+        read_only_fields = ['id']
 
 
 class TeamCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['title']
+        fields = ['id', 'title']
+        read_only_fields = ['id']
