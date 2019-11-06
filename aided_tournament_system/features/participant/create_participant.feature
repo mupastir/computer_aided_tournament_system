a5c46458575f108cc73eb6b8e402f8b2bfde161c
oleg <mupastir@gmail.com>
2019-11-06 10:32:18 +0200

    updated working tests for creation participants views

--- aided_tournament_system/features/participant/create_participant.feature
+++ aided_tournament_system/features/participant/create_participant.feature
@@ -8,23 +8,21 @@ Feature: Create participants
     Then User got success status code
     And User become a player
 
-  Scenario: Create team
-    Given User with email: test and password: top_secure, superuser: True, staff: True .
-    When User create BestTeam
-    Then User got success status code
-
   Scenario: Create referee
     Given User with email: test and password: top_secure, superuser: True, staff: True .
     When User add referee role
     Then User got success status code
     And User become a referee
 
-  Scenario: Add players to team
-    Given User with email: test@test.com and password: extra_strong, superuser: True, staff: True.
-    And Player
-    |first_name|last_name|username      |email        |password   |
-    |Masha     |Test     |test1         |test@test.com|top_secure1|
-    And The TEAM
-    When User add TEST_PLAYER to TEAM
+  Scenario: Create team
+    Given User with email: test and password: top_secure, superuser: True, staff: True .
+    When User create BestTeam
     Then User got success status code
+
+  Scenario: Add user to team
+    Given User with email: test@test.com and password: extra_strong, superuser: True, staff: True.
+    And Player is user
+    And The TEAM
+    When User try to join to TEAM
+    Then Status code is 200
     And TEAM has TEST_PLAYER
--- aided_tournament_system/features/participant/steps/create_participants.py
+++ aided_tournament_system/features/participant/steps/create_participants.py
@@ -1,50 +1,41 @@
 from behave import given, then, when
-
-from features.utils import api_post
+from features.utils import api_post, api_put
 from participant.models import Player, Referee, Team
-from user_auth.models import User
 
 
 @when('User add player role')
 def add_player(context):
-    context.response = api_post('/participant/player/add',
+    context.response = api_post('/participant/player/add/',
                                 context.user)
 
 
 @when('User add referee role')
 def add_referee(context):
-    context.response = api_post('/participant/referee/add',
+    context.response = api_post('/participant/referee/add/',
                                 context.user)
 
 
 @when('User create {team_title}')
 def add_team(context, team_title):
-    context.response = api_post('/participant/team/create',
+    context.response = api_post('/participant/team/create/',
                                 context.user,
                                 {
                                     'title': team_title
                                 })
 
 
-@given('Players')
+@given('Player is user')
 def create_players(context):
-    context.players = []
-    for row in context.table:
-        user = User.objects.create_user(username=row['username'],
-                                        email=row['email'],
-                                        password=row['password'],
-                                        first_name=row['first_name'],
-                                        last_name=row['last_name'])
-        context.players.append(Player.objects.create(user=user.id))
+    Player.objects.create(user_id=context.user.id)
 
 
-@when('User add {player_username} to {team_title}')
-def add_players_to_team(context, player_username, team_title):
-    context.response = api_post(f'/participant/{team_title}/add_player',
-                                context.user,
-                                {
-                                    'player_username': player_username
-                                })
+@when('User try to join to {team_title}')
+def add_players_to_team(context, team_title):
+    context.response = api_put(f'/participant/join_to_team/',
+                               context.user,
+                               {
+                                   'title': team_title
+                               })
 
 
 @given('The {team_title}')
@@ -54,20 +45,14 @@ def create_team(context, team_title):
 
 @then('{team_title} has {player_username}')
 def check_team_has_player(context, team_title, player_username):
-    player = Player.objects.all()
-    team = Team.objects.get(title=team_title)
-    assert True is False
+    assert Player.objects.first().team.first() == Team.objects.first()
 
 
 @then('User become a player')
 def check_player_exist(context):
-    user = context.user
-    players = Player.objects.all()
-    assert True is False
+    assert Player.objects.first().user_id == context.user.id
 
 
 @then('User become a referee')
 def check_referee_exist(context):
-    user = context.user
-    referees = Referee.objects.all()
-    assert True is False
+    assert Referee.objects.first().user_id == context.user.id
