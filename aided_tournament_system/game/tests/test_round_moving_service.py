75774f6d03f3b1bdd04b050f6d42b134238cb49c
oleg <mupastir@gmail.com>
2019-11-15 15:46:28 +0200

    refactored with setup test data

--- aided_tournament_system/game/tests/test_round_moving_service.py
+++ aided_tournament_system/game/tests/test_round_moving_service.py
@@ -13,7 +13,8 @@ from participant.models import Team
 
 class RoundMovingTestCase(TestCase):
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpTestData(cls):
         Team.objects.bulk_create([
             Team(title='indigo', rating=20),
             Team(title='malvin', rating=18),
@@ -23,7 +24,7 @@ class RoundMovingTestCase(TestCase):
             Team(title='carrot', rating=33),
             Team(title='beluga', rating=2)
         ])
-        competition = Competition.objects.create(
+        cls.competition = Competition.objects.create(
             title='Test2',
             start_time=datetime.now(),
             end_time=datetime.now(),
@@ -31,100 +32,99 @@ class RoundMovingTestCase(TestCase):
             schedule_system=ScheduleChoices.TEAM_SYSTEM_OF_8.value,
             gender=GenderChoices.MAN.value
         )
-        schedule_creator = ScheduleCreator(competition.schedule_system,
-                                           competition.id)
+        schedule_creator = ScheduleCreator(cls.competition.schedule_system,
+                                           cls.competition.id)
         schedule_creator.create_schedule()
         for team in Team.objects.all():
-            Application.objects.create(competition=competition,
+            Application.objects.create(competition=cls.competition,
                                        team=team)
-        seeding_teams_service = SeedingTeamsService(competition.id)
+        seeding_teams_service = SeedingTeamsService(cls.competition.id)
         seeding_teams_service.seed()
 
     def test_round_moving(self):
-        competition = Competition.objects.last()
         Game.objects.filter(game_number=1,
-                            competition=competition).update(
+                            competition=self.competition).update(
             home_team_score=2,
             away_team_score=0)
         round_mover = RoundMovingService(
-            Game.objects.get(competition=competition,
+            Game.objects.get(competition=self.competition,
                              game_number=1).id)
         round_mover.move_teams()
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=5
         ).home_team == Team.objects.get(title='carrot')
         Game.objects.filter(game_number=2,
-                            competition=competition).update(
+                            competition=self.competition).update(
             home_team_score=2,
             away_team_score=0
         )
         round_mover = RoundMovingService(
-            Game.objects.get(competition=competition,
+            Game.objects.get(competition=self.competition,
                              game_number=2).id)
         round_mover.move_teams()
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=5
         ).away_team == Team.objects.get(title='cristal')
         Game.objects.filter(game_number=3,
-                            competition=competition).update(
+                            competition=self.competition).update(
             home_team_score=2,
             away_team_score=1
         )
         round_mover = RoundMovingService(
-            Game.objects.get(competition=competition,
+            Game.objects.get(competition=self.competition,
                              game_number=3).id)
         round_mover.move_teams()
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=6
         ).home_team == Team.objects.get(title='indigo')
         Game.objects.filter(game_number=4,
-                            competition=competition).update(
+                            competition=self.competition).update(
             home_team_score=1,
             away_team_score=2
         )
         round_mover = RoundMovingService(
-            Game.objects.get(competition=competition,
+            Game.objects.get(competition=self.competition,
                              game_number=4).id)
         round_mover.move_teams()
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=6
         ).away_team == Team.objects.get(title='croissant')
 
         Game.objects.filter(game_number=5,
-                            competition=competition).update(
+                            competition=self.competition).update(
             home_team_score=1,
             away_team_score=2
         )
         round_mover = RoundMovingService(
-            Game.objects.get(competition=competition,
+            Game.objects.get(competition=self.competition,
                              game_number=5).id)
         round_mover.move_teams()
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=11
         ).home_team == Team.objects.get(title='cristal')
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=9
         ).home_team == Team.objects.get(title='carrot')
         Game.objects.filter(game_number=6,
-                            competition=competition).update(
+                            competition=self.competition).update(
             home_team_score=2,
             away_team_score=1
         )
         round_mover = RoundMovingService(
-            Game.objects.get(competition=competition,
+            Game.objects.get(competition=self.competition,
                              game_number=6).id)
         round_mover.move_teams()
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=12
         ).home_team == Team.objects.get(title='indigo')
         assert Game.objects.get(
-            competition=competition,
+            competition=self.competition,
             game_number=10
         ).home_team == Team.objects.get(title='croissant')
