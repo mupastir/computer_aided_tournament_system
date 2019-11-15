a8954200e1d3de7b9a51d9b19443b3b16f72a883
oleg <mupastir@gmail.com>
2019-11-15 08:55:31 +0200

    fixed adding default rating

--- aided_tournament_system/participant/services/add_rating_to_player.py
+++ aided_tournament_system/participant/services/add_rating_to_player.py
@@ -4,7 +4,7 @@ from participant.models import Player, Rating
 
 def add_rating_to_player(player: Player):
     ratings_list = Rating.objects.bulk_create([
-        Rating(type=CompetitionTypeChoices.BEACH_VOLLEY.value),
-        Rating(type=CompetitionTypeChoices.PARK_VOLLEY.value)
+        Rating(type=CompetitionTypeChoices.BEACH_VOLLEY.value, points=0),
+        Rating(type=CompetitionTypeChoices.PARK_VOLLEY.value, points=0)
     ])
-    player.rating.add(ratings_list)
+    player.rating.add(*ratings_list)
