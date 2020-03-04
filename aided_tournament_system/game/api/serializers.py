a4e1837eb9e1ddaf26fbc3776c32ed57d912b623
oleg <mupastir@gmail.com>
2020-03-04 13:00:27 +0200

    WIP: refactor games api

--- aided_tournament_system/game/api/serializers.py
+++ aided_tournament_system/game/api/serializers.py
@@ -2,8 +2,7 @@ from game.models import Game
 from rest_framework import serializers
 
 
-class UpdateGameScoreSerializer(serializers.ModelSerializer):
+class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
-        fields = ['home_team_score',
-                  'away_team_score']
+        fields = '__all__'
--- aided_tournament_system/game/api/views.py
+++ aided_tournament_system/game/api/views.py
@@ -1,14 +1,31 @@
+from django.shortcuts import get_object_or_404
 from django_utils.permisions import IsRefereeScorerOrAdmin
-from game.api.serializers import UpdateGameScoreSerializer
+from game.api.serializers import GameSerializer
 from game.models import Game
-from rest_framework.generics import UpdateAPIView
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
-class UpdateScoreForGameAPIView(UpdateAPIView):
+class GameAPIView(viewsets.ViewSet):
     permission_classes = (IsRefereeScorerOrAdmin, IsAuthenticated,)
-    serializer_class = UpdateGameScoreSerializer
 
-    def get_queryset(self):
-        pk = self.kwargs['pk']
-        return Game.objects.filter(pk=pk)
+    def list(self, request):
+        queryset = Game.objects.all()
+        serializer = GameSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        queryset = Game.objects.all()
+        game = get_object_or_404(queryset, pk=pk)
+        game.home_team_score = request.data['home_team_score']
+        game.away_team_score = request.data['away_team_score']
+        game.save()
+        serializer = GameSerializer(game)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        queryset = Game.objects.all()
+        game = get_object_or_404(queryset, pk=pk)
+        serializer = GameSerializer(game)
+        return Response(serializer.data, status=status.HTTP_200_OK)
